7cd0baf30a30d3aeea21bf39e721486032774159
Quentin Desbin <quentin.desbin@ynov.com>
2022-11-08 16:36:59 +0100

    use OpenApiValidator on app

--- library-express/app.js
+++ library-express/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const OpenApiValidator = require('express-openapi-validator');
 
 app.use(cors());
 app.use(express.json());
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: './open-api.yaml'
+    })
+);
 
 const booksRouter = require('./routers/books');
 app.use('/books', booksRouter);
