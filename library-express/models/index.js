69dbd89247d8762a13974e1e9ca1df6055e13617
Quentin Desbin <quentin.desbin@ynov.com>
2022-11-11 11:37:36 +0100

    add models

--- library-express/models/index.js
+++ library-express/models/index.js
@@ -2,19 +2,24 @@ const { Sequelize } = require('sequelize');
 const dbConfig = require('../db.config');
 
 // MYSQL :
-/*const instance = new Sequelize(dbConfig.database, dbConfig.username,
-    dbConfig.password, {
+/*const instance = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
         host: dbConfig.hostname,
         dialect: "mysql"
-    });*/
+});*/
 
-// SQLITE3 :
+// SQLITE :
 const instance = new Sequelize({
-    dialect: "sqlite",
-    storage: "./my-db.sqlite"
+    dialect: dbConfig.dialect,
+    storage: dbConfig.storage
 });
 
 module.exports = {
     instance,
-    users: require('./users')(instance)
+    users: require('./users')(instance),
+    books: require('./books')(instance),
+    reviews: require('./reviews')(instance)
 };
+
+// Define associations between models
+instance.models.review.belongsTo(instance.models.user);
+instance.models.review.belongsTo(instance.models.book);
