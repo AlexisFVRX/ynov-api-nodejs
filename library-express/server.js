440c19868f080ca3d8900fd3838aa1be308ad915
Quentin Desbin <quentin.desbin@ynov.com>
2022-12-05 13:24:59 +0100

    create some books by default

--- library-express/server.js
+++ library-express/server.js
@@ -2,8 +2,10 @@ const db = require('./models/index');
 const app = require('./app');
 require('dotenv').config();
 
-db.instance.sync({force: true}).then(() => {
+db.instance.sync({force: true}).then(async () => {
     console.log('Database connected an synchronized');
+
+    await db.books.create({title: "Default Book", date: "2022-12-05"});
 
     app.listen(3000, () => {
         console.log('Server running on port 3000 !');
